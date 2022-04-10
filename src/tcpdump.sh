131457e2d950353cfe63a485f7ea1f8e8fceee25
Vedran Kolka <vedran.kolka@fer.hr>
2022-04-10 20:51:41 +0200

    added capturing on docker network

--- src/tcpdump.sh
+++ src/tcpdump.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/bash
-# brijem da je ovo okej za hvatanje paketa
-sudo tcpdump -i lo -w rezultati.pcap
+# Script to start capturing packets on the given docker network
+function usage {
+    echo "usage: ./tcpdump.sh <docker_network_name> <output_file_name>"
+    echo "    docker_network_name = name of docker network on which to capture packets"
+    echo "    output_file_name    = file name where to write captured data"
+}
+
+if [ $# -lt 2 ]; then
+    usage
+    exit 1
+fi
+# read the arguments
+network_name=$1
+out=$2
+# get gateway ip and interface name
+gateway_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' $network_name)
+interface=$(ip addr | grep $gateway_ip | sed -E "s/\s+/\t/g" | cut -f8)
+
+echo "network_name = $network_name"
+echo "out = $out"
+echo "gateway_ip = $gateway_ip"
+echo "interface = $interface"
+
+tcpdump -i "$interface" -w "$out"
